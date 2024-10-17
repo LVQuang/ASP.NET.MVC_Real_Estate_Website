5167ad54a976bae662e4655befd5f082696b8259
khoadangg23 <khoadangg23@gmail.com>
2024-10-17 16:11:40 +0700

    SE05: Frontend Integrated

--- MVC_Real_Estate_Website/MVC_Real_Estate_Website/App_Start/BundleConfig.cs
+++ MVC_Real_Estate_Website/MVC_Real_Estate_Website/App_Start/BundleConfig.cs
@@ -7,6 +7,32 @@ namespace BaiTapTuan01
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            // css Profile
+            bundles.Add(new StyleBundle("~/bundles/ProfileCSS").Include(
+                "~/Content/ProfileContent/css/bootstrap.min.css",
+                "~/Content/ProfileContent/css/bootstrap-icons.css",
+                "~/Content/ProfileContent/css/apexcharts.css",
+                "~/Content/ProfileContent/css/tooplate-mini-finance.css"));
+
+            // js Profile
+            bundles.Add(new ScriptBundle("~/bundles/ProfileJS").Include(
+                "~/Scripts/ProfileJS/jquery.min.js",
+                //"~/Scripts/ProfileJS/bootstrap.bundle.min.js",
+                "~/Scripts/ProfileJS/apexcharts.min.js",
+                "~/Scripts/ProfileJS/custom.min.js",
+                "~/Scripts/ProfileJS/script.min.js"));
+
+            // js Home
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                "~/Scripts/js/jquery-3.3.1.min.js",
+                "~/Scripts/js/theme-change.js",
+                "~/Scripts/js/jquery.waypoints.min.js",
+                "~/Scripts/js/jquery.countup.js",
+                "~/Scripts/js/owl.carousel.js",
+                "~/Scripts/js/jquery.magnific-popup.min.js",
+                "~/Scripts/js/bootstrap.min.js",
+                "~/Scripts/js/script.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
--- MVC_Real_Estate_Website/MVC_Real_Estate_Website/App_Start/RouteConfig.cs
+++ MVC_Real_Estate_Website/MVC_Real_Estate_Website/App_Start/RouteConfig.cs
@@ -10,6 +10,78 @@ namespace BaiTapTuan01
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Help Center",
+                url: "profile/help-center",
+                defaults: new { controller = "Profile", action = "HelpCenter" }
+            );
+
+            routes.MapRoute(
+                name: "Settings",
+                url: "profile/settings",
+                defaults: new { controller = "Profile", action = "Settings" }
+            );
+
+            routes.MapRoute(
+                name: "Profile",
+                url: "profile/profile",
+                defaults: new { controller = "Profile", action = "Profile" }
+            );
+
+            routes.MapRoute(
+                name: "Wallet",
+                url: "profile/wallet",
+                defaults: new { controller = "Profile", action = "Wallet" }
+            );
+
+            routes.MapRoute(
+                name: "Overview",
+                url: "profile/overview",
+                defaults: new { controller = "Profile", action = "Overview" }
+            );
+
+            routes.MapRoute(
+                name: "Sign Up",
+                url: "signup",
+                defaults: new { controller = "Signin", action = "SignUp" }
+            );
+
+            routes.MapRoute(
+                name: "Sign In",
+                url: "signin",
+                defaults: new { controller = "Signin", action = "Signin" }
+            );
+
+            routes.MapRoute(
+                name: "Contact",
+                url: "contact",
+                defaults: new { controller = "Contact", action = "Contact" }
+            );
+
+            routes.MapRoute(
+                name: "About",
+                url: "about",
+                defaults: new { controller = "About", action = "About" }
+            );
+
+            routes.MapRoute(
+                name: "Detail",
+                url: "detail",
+                defaults: new { controller = "Detail", action = "Detail" }
+            );
+
+            routes.MapRoute(
+                name: "Listing",
+                url: "listing",
+                defaults: new { controller = "Listing", action = "Listing" }
+            );
+
+            routes.MapRoute(
+                name: "Home",
+                url: "home",
+                defaults: new { controller = "Home", action = "Index" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }

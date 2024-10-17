25d3f0c0dedebd4dd957995b2404f9af41776ad1
LVQuang <ql7769663@gmail.com>
2024-10-17 15:58:13 +0700

    SE04: Init Roles and Users

---
+++ MVC_Real_Estate_Website/MVC_Real_Estate_Website/DTO/Request/Register.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Real_Estate_Website.DTO.Request
+{
+    public class Register
+    {
+        [Required(ErrorMessage = "Username can not be empty")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Password can not be empty")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm Password can not be empty")]
+        [Compare("Password", ErrorMessage = "Password and Confirm Password does not match")]
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Email can not be empty")]
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+    }
+}
--- MVC_Real_Estate_Website/MVC_Real_Estate_Website/Startup.cs
+++ MVC_Real_Estate_Website/MVC_Real_Estate_Website/Startup.cs
@@ -3,6 +3,7 @@ using Owin;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security.Cookies;
 using Microsoft.AspNet.Identity.EntityFramework;
+using Real_Estate_Website.Identity;
 
 [assembly: OwinStartup(typeof(Real_Estate_Website.Startup))]
 
@@ -17,6 +18,52 @@ namespace Real_Estate_Website
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
                 LoginPath = new PathString("/Account/Login")
             });
+            this.InitRoleAndUser();
+
+        }
+
+        private void InitRoleAndUser()
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new IdentityDB()));
+            var userManager = new AppUserManger(new AppUserStore(new IdentityDB()));
+
+            InitAdmin(roleManager, userManager);
+            InitCustomerRole(roleManager, userManager);
+        }
+
+        private void InitAdmin(
+            RoleManager<IdentityRole> roleManager, AppUserManger userManger)
+        {
+            if(!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+            }
+
+            if (userManger.FindByName("admin") == null)
+            {
+                var user = new AppUser();
+                user.UserName = "admin";
+                user.Email = "admin@gmail.com";
+                string userPassword = "admin123";
+                var checkUser = userManger.Create(user, userPassword);
+                if (checkUser.Succeeded)
+                {
+                    userManger.AddToRole(user.Id, "Admin");
+                }
+            }
+        }
+
+        private void InitCustomerRole(
+            RoleManager<IdentityRole> roleManager, AppUserManger userManger)
+        {
+            if (!roleManager.RoleExists("Customer"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Customer";
+                roleManager.Create(role);
+            }
         }
     }
 }
